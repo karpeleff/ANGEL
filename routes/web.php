01ca9090d1a252ac0074d371624e821429380bdc
aleksey <alekseykarpelev@gmail.com>
2023-03-18 13:41:06 +1000

    unzip

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('about', function () {
+    return view('about');
+})->name('about');
+Route::get('contact', function () {
+    return view('contact');
+})->name('contact');
+Route::get('galerea', function () {
+    return view('galerea');
+})->name('galerea');
+Route::get('mastera', function () {
+    return view('mastera');
+})->name('mastera');
+Route::get('uslugi', function () {
+    return view('uslugi');
+})->name('uslugi');
+
